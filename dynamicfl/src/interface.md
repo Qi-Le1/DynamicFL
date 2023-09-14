1065946c8f6b16514330b74b50ecaac58e152794
Qi Le <le000288@ahl04.agate.msi.umn.edu>
2023-09-13 20:35:59 -0500

    delete folders

--- dynamicfl/src/interface.md
+++
@@ -1,8 +0,0 @@
-server:
-1. distribute_server_model_to_clients: distribute server model to selected clients
-2. update_server_model: update server model
-3. train: train models of selected clients at each global epoch
-
-client:
-1. create_clients: create list of current clients instance
-2. train: train client model
--- dynamicfl/src/large_scale_train_server_4.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-CUDA_VISIBLE_DEVICES="0" python train_classifier_fl.py --init_seed 0 --world_size 1 --num_experiments 1 --resume_mode 0 --log_interval 0.25 --device cuda --control_name CIFAR10_resnet9_100_non-iid-d-5_fedavg_fix_0.8-0.2_2-1_250&
-CUDA_VISIBLE_DEVICES="1" python train_classifier_fl.py --init_seed 0 --world_size 1 --num_experiments 1 --resume_mode 0 --log_interval 0.25 --device cuda --control_name CIFAR10_resnet9_100_non-iid-l-2_fedavg_fix_0.8-0.2_2-1_250&
-CUDA_VISIBLE_DEVICES="2" python train_classifier_fl.py --init_seed 0 --world_size 1 --num_experiments 1 --resume_mode 0 --log_interval 0.25 --device cuda --control_name CIFAR10_resnet9_100_non-iid-l-5_fedavg_fix_0.8-0.2_2-1_250&
-CUDA_VISIBLE_DEVICES="3" python train_classifier_fl.py --init_seed 0 --world_size 1 --num_experiments 1 --resume_mode 0 --log_interval 0.25 --device cuda --control_name CIFAR10_resnet9_100_non-iid-l-7_fedavg_fix_0.8-0.2_2-1_250&
-wait
